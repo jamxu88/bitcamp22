0dae3e5357962950a578d0ba9af5bcb9ed74c199
Adam L <ghostlyvisuals@gmail.com>
2022-04-09 01:01:04 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import CreateCode from "./routes/CreateCode.js";
 import Data from "./routes/Data.js";
-import Login from "./routes/Login.js";
 import Preferences from "./routes/Preferences.js";
-import Register from "./routes/Register.js";
 import ValidateCode from "./routes/ValidateCode.js";
 
 
@@ -13,9 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use('/', CreateCode);
 app.use('/', Data);
-app.use('/', Login);
 app.use('/', Preferences);
-app.use('/', Register);
 app.use('/', ValidateCode);
 
 app.listen(port, () => {
