cb33ebcce6606769c7ed7b7999a26cda5aba3478
Adam L <49992513+adam6272@users.noreply.github.com>
2022-04-09 17:18:05 -0400

    Update Profile.js

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -1,7 +1,20 @@
 import express from 'express';
 
 let router = express.Router();
-
+import cors from 'cors';
+const allowedOrigins = ['bitcamp2022.herokuapp.com',
+                      'bitcamp2022api.herokuapp.com'];
+router.use(cors({
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  
+  }));
 router.use(function (req, res, next) {
     next();
 });
