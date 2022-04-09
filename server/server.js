b28965557580d0463878ed3b4874e7cc076c57e5
Adam L <ghostlyvisuals@gmail.com>
2022-04-09 03:06:58 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,21 +5,17 @@ import Preferences from "./routes/Preferences.js";
 import ValidateCode from "./routes/ValidateCode.js";
 import Profile from "./routes/Profile.js";
 import AddFunding from "./routes/AddFunding.js";
-<<<<<<< Updated upstream
-import testRoute from "./routes/testRoute.js"
-=======
 import { auth } from 'express-openid-connect';
 
 const config = {
     authRequired: false,
     auth0Logout: true,
     secret: 'Uu7JT95VGCu7MT77f15SnQqwOW6gyZfZ3S9pRysj3hPUq-tZp2Yl1d4nMzWEFeIi',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://bitcamp2022.herokuapp.com:3001',
     clientID: '9fZCjKmRgG35AauwFVoEIpOll7w6fdnR',
     issuerBaseURL: 'https://musepay.us.auth0.com'
 };
 
->>>>>>> Stashed changes
 const app = express();
 const port = 3001;
 app.use(auth(config));
@@ -30,7 +26,7 @@ app.use('/api/', Profile);
 app.use('/api/', Data);
 app.use('/api/', Preferences);
 app.use('/api/', ValidateCode);
-app.use('/test/', testRoute)
+//app.use('/test/', testRoute)
 
 app.listen(port, () => {
     console.log(`Server initialized on port ${port}!`)
