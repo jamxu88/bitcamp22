b44ad4269b065ecdb2cab3c9e40fd9c37de61aad
Adam L <49992513+adam6272@users.noreply.github.com>
2022-04-09 17:18:18 -0400

    Update RemoveFunding.js

--- server/routes/RemoveFunding.js
+++ server/routes/RemoveFunding.js
@@ -3,7 +3,20 @@ import DatabaseManager from '../database/manager.js';
 import CreditCode from "../CreditCode.js"
 let router = express.Router();
 router.use(express.json())
-
+import cors from 'cors';
+const allowedOrigins = ['bitcamp2022.herokuapp.com',
+                      'bitcamp2022api.herokuapp.com'];
+router.use(cors({
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  
+  }));
 router.use(function (req, res, next) {
     console.log('Time:', Date.now());
     next();
