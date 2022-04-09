9037b19575c230dfd1e631f0fe168c0bab5ef1ea
Adam L <49992513+adam6272@users.noreply.github.com>
2022-04-09 17:17:36 -0400

    Update Data.js

--- server/routes/Data.js
+++ server/routes/Data.js
@@ -4,6 +4,20 @@ let router = express.Router();
 router.use(function (req, res, next) {
     next();
 });
+import cors from 'cors';
+const allowedOrigins = ['bitcamp2022.herokuapp.com',
+                      'bitcamp2022api.herokuapp.com'];
+router.use(cors({
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  
+  }));
 
 router.route("/data").get((req, res) => {
     let b1 = Math.floor(Math.random() * 10000);
