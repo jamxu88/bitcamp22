39a737795811af920af687845815d4317abc2626
matthewnanas <spazeplayz@gmail.com>
2022-04-08 23:14:32 -0400

    Landing

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from './Components/Pages/Landing';
 import './Static/Inter-Font/Inter-Regular.ttf';
 import './Static/Inter-Font/Inter-Light.ttf';
