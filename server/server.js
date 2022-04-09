608d3ce42260ac16769483b0514b0d565fa6c67e
Adam L <ghostlyvisuals@gmail.com>
2022-04-09 02:37:25 -0400

    updated

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const config = {
 
 >>>>>>> Stashed changes
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.use(auth(config));
 
 app.use('/api/', AddFunding);
